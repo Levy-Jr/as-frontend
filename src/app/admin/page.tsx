ff8ff1ea36eca73d5c02f689964df1ee7a58c159
Levy-Jr <levybusinessjr@gmail.com>
2024-02-17 14:40:23 -0400

    creating admin painel

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,16 @@
+import * as api from '@/api/server'
+import { AdminPage } from '@/components/admin/AdminPage'
+import { redirect } from 'next/navigation'
+
+const Page = async () => {
+  const logged = await api.pingAdmin()
+  if (!logged) return redirect('/admin/login')
+
+  return (
+    <div>
+      <AdminPage />
+    </div>
+  )
+}
+
+export default Page
